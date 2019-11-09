99e863e32d952433c983441592b742cfdad3ddb9
Alex <lexesss64@gmail.com>
2019-11-09 20:14:44 +0200

    menu

--- src/js/custom/main.js
+++ src/js/custom/main.js
@@ -1,7 +1,13 @@
 $(".basket-btn").click(function(){
-    if(!$(".busket-container").hasClass("active")){
-        $(".busket-container").addClass("active");
+    var busket = $(this).attr("data-open");
+    if(!$(`[data-busket='${busket}']`).hasClass("active")){
+        $(`[data-busket]`).removeClass("active");
+        $(`[data-busket='${busket}']`).addClass("active");
     }
+    else{
+        $(`[data-busket='${busket}']`).removeClass("active"); 
+    }
+    console.log(`[data-busket='${busket}']`)
 });
 
 $(".busket-container .close-triggrer button").click(function(){
@@ -37,4 +43,16 @@ else{
     $(".menu").removeClass("open-catalog");
     $(".catalog").removeClass("active");
 }
+})
+
+$(window).scroll(function(){
+    if($(window).scrollTop() > 5){
+        if(!$(".menu").hasClass("scrolled")) {
+          $(".menu").addClass("scrolled")  
+        }
+        
+    }
+    else{
+        $(".menu").removeClass("scrolled")
+    }
 })
